c150460f7c2a3780a4cab0eff1b1171ef0516ebc
Exolan <97833724+Exolan@users.noreply.github.com>
2023-02-28 11:35:08 +0300

    Add files via upload

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms"
+
+import { Post } from "../post.model";
 
 @Component({
   selector: 'app-post-create',
@@ -7,11 +10,14 @@ import { Component } from "@angular/core";
 })
 
 export class PostCreateComponent{
-  enteredValue = ''
-  newPost = 'NO CONTENT'
+  enteredContent = ''
+  enteredTitle = ''
+  @Output() postCreated = new EventEmitter<Post>()
 
-  onAddPost() {
-
-    this.newPost = this.enteredValue
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+    
+    const post:Post = {title: form.value.title, content: form.value.content}
+    this.postCreated.emit(post);
   }
 }
